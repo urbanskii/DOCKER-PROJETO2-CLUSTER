7553db1901814e00f58df2786dbfa62d6df1af9f
marcos antonio urbanski <marcos.urbanski@gmail.com>
2024-05-29 17:04:17 -0300

    DOCKER-PROJETO2-CLUSTER

---
+++ master.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo docker swarm init --advertise-addr=192.168.56.100
+sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
---
+++ worker.sh
@@ -0,0 +1,1 @@
+    docker swarm join --token SWMTKN-1-6c9zkrajdzdtvpc98g1cch2ejcusgxla05byljxbyxidxusdux-1o1848yxtuv8xega5f6qrojse 192.168.56.100:2377
